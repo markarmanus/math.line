5924c77d04d2449ef7d0997a16c63525cd1f7d98
Mark Armanious <markarmanus@gmail.com>
2019-10-28 19:15:41 -0230

    Linting

--- src/Hints.js
+++ src/Hints.js
@@ -1,40 +1,45 @@
-import React, {Component} from "react";
-import HintsBody from "./HintsBody.js";
+import React, { Component } from "react"
+import HintsBody from "./HintsBody.js"
 
-
-
-class Hints extends Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            isVisible :false,
-            lst:[],
-        }
+class Hints extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isVisible: false,
+      lst: []
     }
-    //function check if the hintsBody is visible if not adds the component to an empty array in the state and re renders Hints Component.
-    help(){
-        if(!this.state.isVisible){
-            this.setState({lst:[<HintsBody key="item.id" level={this.props.level} />],isVisible:true});
+  }
+  //function check if the hintsBody is visible if not adds the component to an empty array in the state and re renders Hints Component.
+  help() {
+    if (!this.state.isVisible) {
+      this.setState({
+        lst: [<HintsBody key="item.id" level={this.props.level} />],
+        isVisible: true
+      })
+    } else {
+      this.setState({ lst: [], isVisible: false })
+    }
+  }
+  //function that closes the removes the HintsBody component from the ( lst  ) state to make sure when user changed level HintsBody is not visible.
+  componentWillReceiveProps(nextProps) {
+    this.setState({ lst: [], isVisible: false })
+  }
 
-        }else{
-            this.setState({lst:[],isVisible:false});
-        }
-    }
-    //function that closes the removes the HintsBody component from the ( lst  ) state to make sure when user changed level HintsBody is not visible.
-    componentWillReceiveProps(nextProps){
-        this.setState({lst:[],isVisible:false})
-    }
-    render(){
-
-        return(
-            <div className="center col-md-4 col-sm-6">
-                <a className="button button-border" id="hintButton" onClick={() => this.help()}>    Help Me!    </a>
-                {this.state.lst}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="center col-md-4 col-sm-6">
+        <a
+          className="button button-border"
+          id="hintButton"
+          onClick={() => this.help()}
+        >
+          {" "}
+          Help Me!{" "}
+        </a>
+        {this.state.lst}
+      </div>
+    )
+  }
 }
 
-
-
-export default Hints;
+export default Hints
